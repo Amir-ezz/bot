69cf8ee84a2cb73f251b8b6385925134a35943af
Amir-ezz <139725214+Amir-ezz@users.noreply.github.com>
2024-04-18 00:52:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,9 @@ stemmer = LancasterStemmer()
 with open("data.pickle", "rb") as f:
     words, labels, training, output = pickle.load(f)
 
-# Load the intents data from JSON file
 with open("intents.json", encoding="utf-8") as file:
     data = json.load(file)
 
-# Load the trained model
 net = tflearn.input_data(shape=[None, len(training[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
@@ -54,33 +52,25 @@ def chat(input_text):
         if tg['tag'] == tag:
             responses = tg['responses']
 
-            # Make sure responses list exists before using it
             if responses:
                 return random.choice(responses)
             else:
                 return "Sorry, I don't understand that."
 
-    # If no matching tag is found, return an error message
     return "Sorry, I don't understand that."
 
-# Initialize an empty list to store chat history
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
 
-# Streamlit UI
 st.title("Chat Bot")
 
-# Text input for user message
 input_text = st.text_input("You:")
 
-# Button to send message
 if st.button("Send"):
     response = chat(input_text)
     st.session_state.chat_history.append(("You:", input_text))
     st.session_state.chat_history.append(("Bot:", response))
-    # Clear input text after sending message
     input_text = ""
 
-# Display chat history
 chat_history_container = st.empty()
 chat_history_container.text_area("Chat History:", value='\n'.join([f"{sender} {message}" for sender, message in st.session_state.chat_history]), height=200)
